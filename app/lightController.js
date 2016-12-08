1503566f5d73f2d37a19edf9bc1458893287817c
tomwilderspin <tomwilderspin@gmail.com>
2016-12-08 15:02:08 +0000

    refactored light control & logic

---
+++ app/lightController.js
@@ -0,0 +1,45 @@
+
+module.exports = (neoPixels) => {
+
+  return (numberOfLights) => {
+    //init the lights control lib
+    neoPixels.init(numberOfLights);
+
+    //define a new list of pixels
+    const pixelData = new Uint32Array(numberOfLights);
+
+    return {
+      updateLight: update(pixelData, neoPixels),
+      resetLights: reset(pixelData, neoPixels),
+      resetDefaults: resetDefaults(neoPixels);
+    };
+  };
+};
+
+//resets all neoPixel bindings & returns it
+const resetDefaults = (neoPixels) => {
+  return () => {
+    neoPixels.reset();
+    return neoPixels;
+  }
+}
+
+//resets all lights to 0 [off]
+const reset = (pixelData, neoPixels) => {
+  return () => {
+    // sets all light values to zero and renders.
+    return neoPixels.render(pixelData.map(() => 0));
+  }
+}
+
+//updates light addressed at index to provided colour
+const updateToColour = (pixelData, neoPixels) => {
+  return (lightIndex, colour) => {
+
+    //update light at index
+    pixelData[lightIndex] = colour;
+
+    //returns the pixelData array passed to render
+    return neoPixels.render(pixelData);
+  }
+};
